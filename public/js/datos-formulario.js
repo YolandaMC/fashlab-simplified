d26e7320b05ad66222ae8a43ec9f2934c7312330
YolandaMC <onandno.project@gmail.com>
2023-06-20 14:06:19 +0200

    Ya se obtienen los datos del formulario correctamente y se guardan en la variable datosFormulario

--- public/js/datos-formulario.js
+++ public/js/datos-formulario.js
@@ -5,48 +5,50 @@ let datosFormulario;
 let datason;
 
 // *FUNCIONES PARA LA RECOGIDA DE LOS DATOS DEL FORMULARIO *//
-const obtenerDatosFormulario = () => {
-	return new Promise((resolve, reject) => {
-		//* RECOGIDA DE LOS DATOS DEL FORMULARIO *//
-		const formulario = document.querySelector('#datos-formulario');
+const tomaDatos = (event) => {
+	//*Para una accion predeterminada del evento
+	event.preventDefault(); // No realiza el proceso que el evento (en este caso le pasamos sumit a esta función sumit) tenga por defecto que es actualizar el formulario
+	// Tomamos el valor de la decision del usuario de enviar sus datos o no al back-end
+	const decisionDatos = document.querySelector('input[type=radio][name=decision-datos]:checked').value;
+	// Convierto el string obtenido a valor booleano (sino "false" lo identifica como booleano true porque es una cadena y no esta vacia)
+	let datos; // creo una variable para introducir los datoe que lleguen del formulario
+	let decisionBoo; // Declaro una variable para recoger la decisión del usuario de mandar sus datos a la base o no
+	if (decisionDatos.toLowerCase() === 'true') {
+		decisionBoo = true;
+	}
+	if (decisionDatos.toLowerCase() === 'false') {
+		decisionBoo = false;
+	}
+	console.log(decisionDatos);
+	console.log(decisionBoo);
+	//* Evaluamos la decisión con un condicional
+	if (!decisionBoo) {
+		alert('Tus datos no serán enviados');
+		// Introduciremos los datos en un objeto (datos) para poder generar nuestro molde
+		const datosFormData = new FormData(event.target); // Pasamos los datos-formulario a un objeto tipo FormData. Target se refiere al form y si apareciese this se referiría al boton (hijos dentro del form que portan el evento)?
+		datos = Object.fromEntries(datosFormData.entries()); // datos es un objeto de Javascript que me sirve para generar mi molde. Prefiero tener fuera de la función los datos para pasarlo al script molde.js
+		console.log(datos);
+		//* Asigno lso datos a mi variable global datosFormulario
+		datosFormulario = datos;
+		//--
+		draw();
 
-		const tomaDatos = (event) => {
-			//*Para una accion predeterminada del evento
-			event.preventDefault(); // No realiza el proceso que el evento (en este caso le pasamos sumit a esta función sumit) tenga por defecto que es actualizar el formulario
-			// Tomamos el valor de la decision del usuario de enviar sus datos o no al back-end
-			const decisionDatos = document.querySelector('input[type=radio][name=decision-datos]:checked').value;
-			// Convierto el string obtenido a valor booleano (sino "false" lo identifica como booleano true porque es una cadena y no esta vacia)
-			let decisionBoo = true; // Doy un valor inicial true
-			if (decisionDatos.toLowerCase() === 'true') {
-				decisionBoo = true;
-			}
-			if (decisionDatos.toLowerCase() === 'false') {
-				decisionBoo = false;
-			}
-			console.log(decisionDatos);
-			console.log(decisionBoo);
-			//* Evaluamos la decisión con un condicional
-			if (!decisionBoo) {
-				alert('Tus datos no serán enviados');
-				// Introduciremos los datos en un objeto (datos) para poder generar nuestro molde
-				const datosFormData = new FormData(event.target); // Pasamos los datos-formulario a un objeto tipo FormData. Target se refiere al form y si apareciese this se referiría al boton (hijos dentro del form que portan el evento)?
-				datosFormulario = Object.fromEntries(datosFormData.entries()); // datos es un objeto de Javascript que me sirve para generar mi molde. Prefiero tener fuera de la función los datos para pasarlo al script molde.js
-				console.log(datosFormulario);
-			} else {
-				alert('Tus datos serán enviados');
-				//* FormDataAPI es una Api que nos provee formas de obtener datos de los formularios
-				// Constructor que crea un objeto de tipo FormData
-				const datosFormData = new FormData(event.target);
-				// El método Object.fromEntries() transforma una lista de pares con [clave-valor] en un objeto.
-				datosFormulario = Object.fromEntries(datosFormData.entries()); // Datos es un objeto de Javascript que me sirve para generar mi molde
-				// const datason = JSON.stringify(datos); // Meto los datos en formato JSON en la variable datason para ser enviados al back-end
-				console.log(datosFormulario);
-				//console.log(datason);
-			}
-			resolve(datosFormulario); //!pido que me devuelva los datos para enviarlos al script molde.js
-		};
-		formulario.addEventListener('submit', tomaDatos);
-	});
+		//--
+	} else {
+		alert('Tus datos serán enviados');
+		//* FormDataAPI es una Api que nos provee formas de obtener datos de los formularios
+		// Constructor que crea un objeto de tipo FormData
+		const datosFormData = new FormData(event.target);
+		// El método Object.fromEntries() transforma una lista de pares con [clave-valor] en un objeto.
+		datos = Object.fromEntries(datosFormData.entries()); // Datos es un objeto de Javascript que me sirve para generar mi molde
+		//const datason = JSON.stringify(datos); // Trasformo los datos a formato JSON en la variable datason para ser enviados al back-end
+		console.log(datos);
+		//* Asigno lso datos a mi variable global datosFormulario
+		datosFormulario = datos;
+		//--
+		draw();
+		//--
+	}
 };
 
 /*Funcion para extraer un solo dato del formulario (NO SE VA A EMPLEAR)*/
@@ -62,18 +64,13 @@ const obtenerDatosFormulario = () => {
 // 	console.log({ edad });
 // };
 
-//* RECOGIDA DE LOS DATOS DEL FORMULARIO *//
-//const formulario = document.querySelector('#datos-formulario'); //meto el formulario en una variable para acceder a el cuando lo desee
-// Ahora mediante el metodo addEventListener  ejecuto la función toma Datos cuando el evento submit del formulario se produzca
-//formulario.addEventListener('submit', tomaDatos); //funciones del archivo js/toma-medidas.js. Toma los datos y los pone en formato JSON para enviarlos al Back-end
-//datosFormulario = formulario.addEventListener('submit', tomaDatos); // meto los datos devueltos a un objeto datosFormulario
+// 	//* RECOGIDA DE LOS DATOS DEL FORMULARIO *//
+// Promesa para obtener los datos del formulario
+// Escucha el evento 'submit' del botón del formulario y llama a la función 'tomaDatos'
+const formulario = document.querySelector('#datos-formulario');
+formulario.addEventListener('submit', tomaDatos);
 
-//TODO pensar si mandarlos ya en formato JSON o mandarlos como objeto y pasarlos a JSON cuadno se estriban alli
-datason = JSON.stringify(datosFormulario); // Meto los datos en formato JSON en la variable datason para ser enviados al back-end
-console.log(datason);
-
-//* ESTOS DATOS SE ENVIARAN EN UN OBJETO AL SCRIPT ./construccion-body-viewer/molde.js PARA QUE SE GENERE EL PATRON CON ELLOS*//
-//export default { obtenerDatosFormulario }; //! EXPORTO ESTA FUNCION
-//module.exports = { obtenerDatosFormulario };
-
-export default obtenerDatosFormulario;
+function draw() {
+	console.log('Ya podemos pasar al siguiente apartado');
+	console.log(datosFormulario);
+}
--- public/js/reactividad-web.js
+++ public/js/reactividad-web.js
@@ -2,56 +2,59 @@
 
 //Código JS correcpondiente al botón de html con id #btn-ampliar que permite ampliar y reducir el canvas #lienzo
 
-const container = document.querySelector('.container');
-const ampliarLienzo = document.querySelector('#lienzo');
-const btnAmpliar = document.querySelector('#btn-ampliar');
+// const container = document.querySelector('.container');
+// const ampliarLienzo = document.querySelector('#lienzo');
+// const btnAmpliar = document.querySelector('#btn-ampliar');
 
-// Función que me permite ampliar el canvas #lienzo y su .container y que el botón me permita reducir despues cambiando la función del atributo onclik
-function ampliar() {
-	container.style.width = '100%';
-	container.style.height = '100vh';
-	container.style.margin = '0';
+// // Función que me permite ampliar el canvas #lienzo y su .container y que el botón me permita reducir despues cambiando la función del atributo onclik
+// function ampliar() {
+// 	container.style.width = '100%';
+// 	container.style.height = '100vh';
+// 	container.style.margin = '0';
 
-	ampliarLienzo.style.width = '100%';
-	ampliarLienzo.style.height = '100vh';
-	ampliarLienzo.style.backgroundSize = 'cover';
-	ampliarLienzo.style.backgroundRepeat = 'no-repeat';
+// 	ampliarLienzo.style.width = '100%';
+// 	ampliarLienzo.style.height = '100vh';
+// 	ampliarLienzo.style.backgroundSize = 'cover';
+// 	ampliarLienzo.style.backgroundRepeat = 'no-repeat';
 
-	// Cambio el texto que aparece en el botón
-	btnAmpliar.innerHTML = 'Reducir pantalla';
-	// Cambio su posición
-	btnAmpliar.style.position = 'fixed';
-	btnAmpliar.style.top = '10px';
-	btnAmpliar.style.left = '10px';
-	btnAmpliar.style.zIndex = '1';
-	// Cambio la función del atributo onclick de ampliar a reducir
-	btnAmpliar.setAttribute('onclick', 'reducir()');
-}
+// 	// Cambio el texto que aparece en el botón
+// 	btnAmpliar.innerHTML = 'Reducir pantalla';
+// 	// Cambio su posición
+// 	btnAmpliar.style.position = 'fixed';
+// 	btnAmpliar.style.top = '10px';
+// 	btnAmpliar.style.left = '10px';
+// 	btnAmpliar.style.zIndex = '1';
+// 	// Cambio la función del atributo onclick de ampliar a reducir
+// 	btnAmpliar.setAttribute('onclick', 'reducir()');
+// }
 
-// Función que me permite reducir el canvas #lienzo y su .container y que el botón me permita ampliar despues cambiando la función del atributo onclik
-function reducir() {
-	//Dimensiones originales canvas #lienzo y .container
-	container.style.width = '840px';
-	container.style.height = '700px';
-	// container.style.margin = '20px auto'; margen añadido al padre section
+// // Función que me permite reducir el canvas #lienzo y su .container y que el botón me permita ampliar despues cambiando la función del atributo onclik
+// function reducir() {
+// 	//Dimensiones originales canvas #lienzo y .container
+// 	container.style.width = '840px';
+// 	container.style.height = '700px';
+// 	// container.style.margin = '20px auto'; margen añadido al padre section
 
-	ampliarLienzo.style.width = '840px';
-	ampliarLienzo.style.height = '700px';
+// 	ampliarLienzo.style.width = '840px';
+// 	ampliarLienzo.style.height = '700px';
 
-	// Cambio el texto que aparece en el botón
-	btnAmpliar.innerHTML = 'Ampliar pantalla';
-	// Cambio su posición
-	btnAmpliar.style.position = 'relative';
-	btnAmpliar.style.top = '0';
-	btnAmpliar.style.left = '0';
-	btnAmpliar.style.zIndex = '0';
-	// Cambio la función del atributo onclick de ampliar a reducir
-	btnAmpliar.setAttribute('onclick', 'ampliar()');
-}
+// 	// Cambio el texto que aparece en el botón
+// 	btnAmpliar.innerHTML = 'Ampliar pantalla';
+// 	// Cambio su posición
+// 	btnAmpliar.style.position = 'relative';
+// 	btnAmpliar.style.top = '0';
+// 	btnAmpliar.style.left = '0';
+// 	btnAmpliar.style.zIndex = '0';
+// 	// Cambio la función del atributo onclick de ampliar a reducir
+// 	btnAmpliar.setAttribute('onclick', 'ampliar()');
+// }
 
-//Código JS correspondiente a los slider de html que permite visualizar el número que se está introduciendo
+//-----------
 
-//Función que actualiza el valor del slider a cada movimiento del usuario
+//-----------
+
+//*Código JS correspondiente a los slider de html que permite visualizar el número que se está introduciendo
+// Función que actualiza el valor del slider a cada movimiento del usuario
 function updateRange() {
 	const limit = this.parentElement.getElementsByClassName('limit')[0];
 	limit.innerHTML = this.value;
